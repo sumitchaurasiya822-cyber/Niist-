548703a6aad4ebe1e0987bd35710a01b33b9a6c7
sumitchaurasiya822-cyber <sumitchaurasiya822@gmail.com>
2026-04-20 20:46:36 +0530

    Fix formatting of student names in dashboard

--- login.js
+++ login.js
@@ -1,8 +1,4 @@
-const SECRET_CODE = "cse2025";
-
-function login(){
-
- let val = document.getElementById("input").value.trim().toLowerCase();  const students = ["sumit Kuamr Chaurasiya","Nikhil Dubey","Jay Srivastav","Dhananjay Srivastav","Hemant Kushwah","Jitendra Chouhan,"Harinarayan Dwivedi"," Gyanendra Singh","Bhaskar Dwivedi","Niranjan","Suhani Yaduwanshi","Muskan Lodhi","Surbhi Lodhi","Sakshi Bisen","Sakshi Kumari","Gaurav kushwaha","Vikash Baudh","Stayam Singh","Sanjan Patle","Rahul","Rama Kushwaha","Poonam Vaishy","Jyoti Dubey","Anamika Verma","Jyotika Singh","Harshita Singh","Diksha Paradhi","Anjali Paradhi",Mohit Dwivedi","Shivam","Sumit Pawar","Raunak Dubey","Pranav Gupta","Aniket Singh","Khushi Roi","Ashish Roi","Rajveer Singh","Poorvi Rawat"Rishav Patel","Monika Kashyap","Swati Kumari","Himanshu Thakre","Nikunj Nagoria",];
+const SECRET_CODE = function login() let val = document.getElementById("input").value.trim().toLowerCase();  const students = ["sumit kumar chaurasiya","nikhil dubey","jay srivastav","dhananjay srivastav","hemant kushwah","jitendra couhan","harinarayan dwivedi"," gyanendra singh","bhaskar dwivedi","niranjan","suhani yaduwanshi","muskan lodhi","surbhi lodhi","sakshi bisen","sakshi kumari","gaurav kushwaha","vikash baudh","stayam singh","sanjan patle","rahul","rama kushwaha","poonam vaishy","jyoti dubey","anamika verma","jyotika singh","harshita singh","diksha paradhi","anjali paradhi","mohit dwivedi","shivam","sumit pawar","raunak dubey","pranav gupta","aniket singh","khushi roi","ashish roi","rajveer singh","poorvi rawat","rishav patel","monika kashyap","swati kumari","himanshu thakre","nikunj nagoria",];
 
   if(val === SECRET_CODE){
     localStorage.setItem("mode","all");
