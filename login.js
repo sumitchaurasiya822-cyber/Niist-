ab7f2630ec3de4b749f81691a05c9be099e03d24
sumitchaurasiya822-cyber <sumitchaurasiya822@gmail.com>
2026-04-20 17:52:39 +0530

    Fix formatting of student names in dashboard.js

--- login.js
+++ login.js
@@ -2,9 +2,7 @@ const SECRET_CODE = "cse2025";
 
 function login(){
 
-  let val = document.getElementById("input").value.trim().toLowerCase();
-
-  const students = ["sumit","rahul","aman","neha"];
+ let val = document.getElementById("input").value.trim().toLowerCase();  const students = ["sumit Kuamr Chaurasiya","Nikhil Dubey","Jay Srivastav","Dhananjay Srivastav","Hemant Kushwah","Jitendra Chouhan,"Harinarayan Dwivedi"," Gyanendra Singh","Bhaskar Dwivedi","Niranjan","Suhani Yaduwanshi","Muskan Lodhi","Surbhi Lodhi","Sakshi Bisen","Sakshi Kumari","Gaurav kushwaha","Vikash Baudh","Stayam Singh","Sanjan Patle","Rahul","Rama Kushwaha","Poonam Vaishy","Jyoti Dubey","Anamika Verma","Jyotika Singh","Harshita Singh","Diksha Paradhi","Anjali Paradhi",Mohit Dwivedi","Shivam","Sumit Pawar","Raunak Dubey","Pranav Gupta","Aniket Singh","Khushi Roi","Ashish Roi","Rajveer Singh","Poorvi Rawat"Rishav Patel","Monika Kashyap","Swati Kumari","Himanshu Thakre","Nikunj Nagoria",];
 
   if(val === SECRET_CODE){
     localStorage.setItem("mode","all");
